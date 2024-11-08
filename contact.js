e7b314fb3f43f99eaf29a19c3c56c6a34306cff3
subhankarray77 <chudasma111555@gmail.com>
2024-11-08 20:10:34 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_sx3gj89';
+   const templateID = 'template_w88lhh7';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
